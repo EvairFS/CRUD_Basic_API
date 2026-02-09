0b2aad46ffa6d53e3f007cdd0b46d64d3940d714
Evair F S Filho <evair.juninho@gmail.com>
2026-02-09 11:50:10 -0400

    Update, agora o Login Funciona.

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
 import { UsuarioModule } from './usuario/usuario.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [PrismaModule, UsuarioModule],
+  imports: [PrismaModule, UsuarioModule, AuthModule],
 })
 export class AppModule {}
